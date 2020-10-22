10a0cba3874bcaec039fa6ebd4f04754af5af34e
duytranndev <71779101+duytranndev@users.noreply.github.com>
2020-10-22 21:08:57 +0700

    create CSDL and FileXuLy

--- INSTANTBOOKING/Home.aspx.cs
+++ INSTANTBOOKING/Home.aspx.cs
@@ -4,15 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using INSTANTBOOKING.App_Code;
+
 namespace INSTANTBOOKING
 {
+    
     public partial class Home : System.Web.UI.Page
     {
+        FileXuLy xuly = new FileXuLy();
 
         protected void Page_Load(object sender, EventArgs e)
         {
-		
-
+            if (Page.IsPostBack) return;
+            string[] values = new string[] { };
+            string[] parameter = new string[] { };
+            DataList1.DataSource = xuly.docAllDLStored("getAllRoom", values, parameter);
+            DataList1.DataBind();
 		}
-    }
+
+		
+	}
+
+	
 }
